ba9e03afc4b00df291241e94234404d980c1d922
Madison Hershkovitz <mlhersh6@gmail.com>
2019-03-03 16:36:59 -0600

    support component outline working

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -3,12 +3,33 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 
 class Support extends Component {
+  handleClick = () => {
+    console.log('button clicked');
+    
+    axios({
+      method: 'POST',
+      url: '/support',
+      data: this.props,
+    }).then ((response) =>{
+        console.log(response);
+    }).catch ((error) => {
+      console.log('could not add support rating', error);
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+            ...this.props,
+            support: event.target.value,
+    })
+}
+
   render() {
     return (
       <form>
         <div>
-          <h1>How are you feeling today?</h1>
-          Feeling?
+          <h1>How well are you being supported?</h1>
+          Support?
           <input 
           placeholder="1 - 5"
           value={this.data}
